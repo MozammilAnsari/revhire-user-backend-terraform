75681b242ab71eb0a28c93d75120fdcac9479612
Mozammil Ansari <modassirrr@gmail.com>
2024-06-03 07:51:42 +0530

    add

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "s3-revhire-backend-user-remote"
+    bucket         = "s3-backend-for-revhire-infrastructure-creation-1"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
